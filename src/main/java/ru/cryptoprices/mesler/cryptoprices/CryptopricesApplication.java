336752afd8a4244adcabb9eaded36c60d5ceeef8
Roman Mesler <pay2mesler@gmail.com>
2024-03-07 10:15:10 +0500

    init

---
+++ src/main/java/ru/cryptoprices/mesler/cryptoprices/CryptopricesApplication.java
@@ -0,0 +1,13 @@
+package ru.cryptoprices.mesler.cryptoprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptopricesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CryptopricesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/cryptoprices/mesler/cryptoprices/CryptopricesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.cryptoprices.mesler.cryptoprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptopricesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
